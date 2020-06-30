b3b9abd8b504596d009b77325e3ea62fe42c78b3
Ignace <ignace.hakizimana@aos.rw>
2020-07-01 01:33:19 +0200

    Initial Project Add

---
+++ src/test/java/rw/aos/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package rw.aos.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
