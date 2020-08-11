d863e58add973e15b602aecc5aed38fb08eb6ea2
Ignace <ignace.hakizimana@aos.rw>
2020-08-11 20:44:29 +0200

    Create Visit Entity. closes #26

--- pet-clinic-data/src/main/java/rw/aos/sfgpetclinic/model/Owner.java
+++ pet-clinic-data/src/main/java/rw/aos/sfgpetclinic/model/Owner.java
@@ -1,8 +1,19 @@
 package rw.aos.sfgpetclinic.model;
 
+import java.util.Set;
+
 /**
  * @author : Ignace
  * @date : Mon, 7/13/2020
  **/
 public class Owner extends Person {
+    private Set<Pet> pets;
+
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
