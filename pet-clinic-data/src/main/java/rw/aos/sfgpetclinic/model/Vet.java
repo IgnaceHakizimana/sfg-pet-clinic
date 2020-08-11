ffd05de78738edae025939fb408694360d976fd4
Ignace <ignace.hakizimana@aos.rw>
2020-08-11 20:51:32 +0200

    Create Vet Speciality Entity, Associate to Vet. closes #27

--- pet-clinic-data/src/main/java/rw/aos/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/rw/aos/sfgpetclinic/model/Vet.java
@@ -1,8 +1,19 @@
 package rw.aos.sfgpetclinic.model;
 
+import java.util.Set;
+
 /**
  * @author : Ignace
  * @date : Mon, 7/13/2020
  **/
 public class Vet extends Person{
+    private Set<Speciality> specialities;
+
+    public Set<Speciality> getSpecialities() {
+        return specialities;
+    }
+
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
+    }
 }
