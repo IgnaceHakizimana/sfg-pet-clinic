910811ff93ddd4c203f5496838c5b4827c1d918d
Ignace <ignace.hakizimana@aos.rw>
2020-08-16 23:13:48 +0200

    Converts Vets to JPA Entities. closes #37

--- pet-clinic-data/src/main/java/rw/aos/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/rw/aos/sfgpetclinic/model/Vet.java
@@ -1,5 +1,6 @@
 package rw.aos.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -7,7 +8,11 @@ import java.util.Set;
  * @author : Ignace
  * @date : Mon, 7/13/2020
  **/
+@Entity
+@Table(name = "vets")
 public class Vet extends Person{
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "vet_specialities", joinColumns = @JoinColumn(name = "vet_id"), inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialities = new HashSet<>();
 
     public Set<Speciality> getSpecialities() {
