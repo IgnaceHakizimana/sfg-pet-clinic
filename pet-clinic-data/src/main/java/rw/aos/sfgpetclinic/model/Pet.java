1c7d1eccbc66e33f4ab84a3d61ec592d82c58bf9
Ignace <ignace.hakizimana@aos.rw>
2020-08-16 23:21:22 +0200

    Create Visit Entity. closes #38

--- pet-clinic-data/src/main/java/rw/aos/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/rw/aos/sfgpetclinic/model/Pet.java
@@ -1,17 +1,16 @@
 package rw.aos.sfgpetclinic.model;
 
-import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * @author : Ignace
  * @date : Mon, 7/13/2020
  **/
 @Entity
-@Table(name ="pets")
+@Table(name = "pets")
 public class Pet extends BaseEntity {
     private String name;
 
@@ -24,6 +23,8 @@ public class Pet extends BaseEntity {
     private Owner owner;
 
     private LocalDate birthDate;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
     public String getName() {
         return name;
@@ -55,6 +56,14 @@ public class Pet extends BaseEntity {
 
     public void setBirthDate(LocalDate birthDate) {
         this.birthDate = birthDate;
+    }
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
     }
 
     @Override
