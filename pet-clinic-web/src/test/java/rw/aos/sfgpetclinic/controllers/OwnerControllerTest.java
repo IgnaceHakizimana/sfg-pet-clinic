e0f6087dbc95e1273b0f76e32d29da03f0e063ae
Ignace <ignace.hakizimana@aos.rw>
2020-09-09 05:57:03 +0200

    Write Spring MVC Tests for Owner Controller. closes #52

---
+++ pet-clinic-web/src/test/java/rw/aos/sfgpetclinic/controllers/OwnerControllerTest.java
@@ -0,0 +1,61 @@
+package rw.aos.sfgpetclinic.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import rw.aos.sfgpetclinic.model.Owner;
+import rw.aos.sfgpetclinic.services.OwnerService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.hamcrest.Matchers.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(MockitoExtension.class)
+public class OwnerControllerTest {
+    @Mock
+    OwnerService ownerService;
+
+    @InjectMocks
+    OwnerController controller;
+
+    Set<Owner> owners;
+
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        owners = new HashSet<>();
+        owners.add(Owner.builder().id(1l).build());
+        owners.add(Owner.builder().id(2l).build());
+
+        mockMvc = MockMvcBuilders
+                .standaloneSetup(controller)
+                .build();
+    }
+
+    @Test
+    public void listOwners() throws Exception {
+    }
+
+    @Test
+    void findOwners() throws Exception {
+        mockMvc.perform(get("/owners/find"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("notimplemented"));
+                //.andExpect(model().attributeExists("owner"));
+
+        verifyZeroInteractions(ownerService);
+    }
+}
